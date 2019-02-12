d93648dd6343baf9203f2be8c0488989465fd8e1
Kathryn Carr <kathrynelizabethcarr@gmail.com>
2019-02-12 15:29:01 -0800

    Add ALREDI logos

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ActiveRecord::Base
 
   accepts_nested_attributes_for :user_externals, allow_destroy: true
 
-  validates_presence_of :encrypted_password, :ls_list_state
+  validates_presence_of :encrypted_password, :ls_list_state, :full_name
   validates_presence_of :username, uniqueness: { case_sensitive: false }
   validates_presence_of :email, uniqueness: { case_sensitive: false }
   validates :ls_list_state, inclusion: {
