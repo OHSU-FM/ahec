34c54585428699228ccd822a7a2e7cceb538eb70
Kathryn Carr <kathrynelizabethcarr@gmail.com>
2019-04-10 13:00:08 -0700

    Remove creation of Users in database seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
-a = User.create!(username: 'admin', email: 'admin@test.com', password: 'password', is_admin: true)
-s = User.create!(username: 'student', email: 'student@test.com', password: 'password')
+# Crete User in console with original passwords
+# a = User.create!(username: 'admin', email: 'admin@test.com', password: 'password', is_admin: true)
 pg = PermissionGroup.create!(title: 'test pg')
-cohort = Cohort.create!(owner: c, users: [s], permission_group: pg)
-ct = CommentThread.create!(commentable: cohort, user: s)
